5d6f12b25d05645e67d4f4e9397566c6a331ced4
MarcosPauloJS <marcospaulooj@gmail.com>
2024-07-28 22:20:37 -0300

    configuracao do styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import styled from 'styled-components';
 import './App.css'
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: ${(props) => props.theme.color.secondary};
+`;
 function App() {
   return (
-    <>
+    <Title>
       ibbx desafio
-    </>
+    </Title>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  color: {
+    primary:  '#263440',
+    secondary: '#BD783C',
+    textDark: '#12161A',
+    textLight: '#FFF',
+    background: "#141827",
+  },
+
+  border: {
+    radius: "10px",
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+
+    </ThemeProvider>
   </React.StrictMode>,
 )
